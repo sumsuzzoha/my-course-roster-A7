228668dd6cdfc61afbfaf42146d3e61434ed92c0
Md. Sumsuzzoha <mdzoha11@gmail.com>
2023-11-02 02:46:33 +0600

    course select validate & remaining credit added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,17 +1,51 @@
+import PropTypes from "prop-types";
 import './Cart.css'
 
-const Cart = () => {
+const Cart = ({ creditHours, totalPrice, selectedCourses }) => {
+    const remainingCrHours = 20 - creditHours;
+
+    console.log(selectedCourses);
+
     return (
         <div className="lg:w-1/3 my-4 lg:my-0 bg-white rounded-lg mx-auto p-4 cart-body">
-            <h3 className="my-6 mx-auto border-b-black">Credit Hours Remaining {} hr</h3>
+            <h3 className="my-6 mx-auto border-b-black">Credit Hours Remaining {remainingCrHours} hr</h3>
             <hr />
-            <h3 className="my-4">Course Name</h3>
+            <div>
+
+                <h3 className="my-4">Course Name</h3>
+                {/* <p>{selectedCourses.length}</p> */}
+                
+
+                <ol>
+                    {/* <ul>
+                        {items.map((item, index) =>
+                            <li key={index}>
+                                {`Item ${index + 1}: ${item}`}
+                            </li>
+                        )}
+                    </ul> */}
+                </ol>
+
+                <ol>
+                    {
+                        selectedCourses.map(course =><li key={course.id}>{course.title}</li>)
+                    }
+                </ol>
+
+
+
+            </div>
             <hr />
-            <h5 className='my-4'>Total Credit Hour: {}</h5>
+            <h5 className='my-4'>Total Credit Hour: {creditHours}</h5>
             <hr />
-            <h5 className='my-4'>Total Price: b {} USD</h5>
+            <h5 className='my-4'>Total Price: {totalPrice} USD</h5>
         </div>
     );
-}; 
+};
+Cart.propTypes = {
+    creditHours: PropTypes.number.isRequired,
+    totalPrice: PropTypes.number.isRequired,
+    selectedCourses: PropTypes.array.isRequired
+}
 
 export default Cart;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,20 +4,44 @@ import Courses from "../Courses/Courses";
 
 const Home = () => {
 
- const [selectedCourses, setSelectedCourses] = useState([]);
+    const [selectedCourses, setSelectedCourses] = useState([]);
+    const [creditHours, setCreditHours] = useState(0);
+    const [totalPrice, setTotalPrice] = useState(0);
 
     const handleSelectCourse = course => {
+        const { credit_hours, price } = course;
+
+        if ((creditHours + credit_hours) > 20) {
+            alert('This Course is Exceed the Reamining Credit Limit')
+            return;
+        }
+
+        for (const obj of selectedCourses) {
+            if (obj.id === course.id) {
+                alert('This Course Already Selected')
+                return;
+            }
+        }
         const newCoursesArray = [...selectedCourses, course];
         setSelectedCourses(newCoursesArray);
-        // console.log(course);
+
+        const sumCreditHours = creditHours + credit_hours;
+        setCreditHours(sumCreditHours);
+
+
+        const nePrice = totalPrice + price;
+        setTotalPrice(nePrice);
     }
-    console.log(selectedCourses);
 
     return (
         <div className="lg:mx-auto">
             <h3 className="text-3xl	font-bold my-8">Course Registration</h3>
             <div className="lg:flex lg:flex-row-reverse gap-3 lg:mx-4 ">
-                <Cart></Cart>
+                <Cart
+                    creditHours={creditHours}
+                    totalPrice={totalPrice}
+                    selectedCourses={selectedCourses}
+                ></Cart>
                 <Courses
                     handleSelectCourse={handleSelectCourse}
                 ></Courses>
